b65df9b18e426ee7c734d47400b7ff923c231c1d
yetun <yetun@MSI>
2022-03-29 23:03:26 -0400

    v2

--- src/module-info.java
+++ src/module-info.java
@@ -1,11 +1,11 @@
 module lab3 {
-	exports lab3;
-	requires javafx.base;
-	requires javafx.fxml;
-	requires javafx.graphics;
-	requires javafx.controls;
-	requires javafx.media;
-	requires javafx.swing;
-	requires javafx.swt;
-	requires javafx.web;	
+		requires javafx.base;
+		requires javafx.fxml;
+		requires javafx.graphics;
+		requires javafx.controls;
+		requires javafx.media;
+		requires javafx.swing;
+		requires javafx.swt;
+		requires javafx.web;	
+		exports lab3;
 }
